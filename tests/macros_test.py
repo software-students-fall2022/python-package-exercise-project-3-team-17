2c55f68729072e5c54ab7ab1db3bd376ba38e6d6
bpointi <53314975+bpointi@users.noreply.github.com>
2022-11-07 21:51:27 -0500

    testing for macros

--- tests/macros_test.py
+++ tests/macros_test.py
@@ -1,6 +1,30 @@
 from src.amIFat.macros import *
+import pytest
+import math
 
 def test_calculateREE():
-    # print(10 * 80 + 6.25 * 175 - 5 * 21 + 5)
-    # print(calculateREE(21, 'm', 175, 80, 'm'))
     assert calculateREE(21, 'm', 175, 80, 'm') == 1793.75
+
+def test_raiseExceptionGenderREE():
+     with pytest.raises(Exception):
+        calculateREE(45, 'x', 123, 78, 'm')
+
+def test_raiseExceptionScaleREE():
+         with pytest.raises(Exception):
+            calculateREE(12, 'm', 571, 345678, 'x')
+
+def test_validDimensions():
+    with pytest.raises(Exception):
+        validDimensions(21, 21, 1401)
+    with pytest.raises(Exception):
+        validDimensions(282, 21, 21)
+    with pytest.raises(Exception):
+        validDimensions(150, 1500, 21)
+
+def test_calculateTDEE():
+    ree = calculateREE(29, 'm', 183, 88, 'm')
+    assert math.ceil(calculateTDEE(ree, 4)) == 3250
+
+# def test_raiseExceptionTDEE():
+#     with pytest.raises(Exception):
+#         calculateTDEE(68, )
